57970fb264dbd69ec4d1497b18617c9e1d636aaf
Glaze <i@inevitable.tech>
2022-03-19 21:41:06 +0800

    fix warning

--- src/App.js
+++ src/App.js
@@ -1,48 +1,53 @@
-import './App.css';
-import CategoryCard from './components/CategoryCard.js';
-import {
-  useQuery,
-  gql
-} from "@apollo/client";
-
+import "./App.css";
+import CategoryCard from "./components/CategoryCard.js";
+import { useQuery, gql } from "@apollo/client";
 
 const queryTags = gql`
-query AllTags{
-  __type(name: "Tags") {
-    enumValues {
-      name
+  query AllTags {
+    __type(name: "Tags") {
+      enumValues {
+        name
+      }
     }
   }
-}
-`
+`;
 
 function App() {
   const { loading, error, data } = useQuery(queryTags);
   // sortedTags to control the order of the tags and we will append other tags at the end of the page
-  const sortedTags = ['Promoted', 'CEX', 'DEX']
+  const sortedTags = ["Promoted", "CEX", "DEX"];
   if (loading) return null;
   if (error) return `Error! ${error}`;
   if (!data) return null;
   return (
     <div>
       <div className="terminal-logo" style={{ marginLeft: "2em" }}>
-        <div className="logo terminal-prompt"><a href="https://twitter.com/unblock256" target="_blank" className="no-style">un.Block</a></div>
+        <div className="logo terminal-prompt">
+          <a
+            href="https://twitter.com/unblock256"
+            target="_blank"
+            rel="noreferrer"
+            className="no-style"
+          >
+            un.Block
+          </a>
+        </div>
       </div>
-      <div className='container'>
-        {
-          sortedTags.map(
-            tag => <CategoryCard key={tag} tag={tag}></CategoryCard>
-          )
-        }
-        {data.__type.enumValues.map(item => {
+      <div className="container">
+        {sortedTags.map((tag) => (
+          <CategoryCard key={tag} tag={tag}></CategoryCard>
+        ))}
+        {data.__type.enumValues.map((item) => {
           if (!sortedTags.includes(item.name)) {
-            return (< CategoryCard key={item.name} tag={item.name}></CategoryCard>)
+            return (
+              <CategoryCard key={item.name} tag={item.name}></CategoryCard>
+            );
           }
-        })
-        }
-      </div >
+          return null;
+        })}
+      </div>
     </div>
-  )
+  );
 }
 
 export default App;
